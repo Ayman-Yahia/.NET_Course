ca92571abeed7929952665d010b2e734d29b377e
Ayman Yahia <ayman.ya1997@gmail.com>
2021-09-29 14:53:10 +0300

    user input

---
+++ C#Learning/userInput/userInput/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace userInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("whats your name?");
+            String name = Console.ReadLine();
+            Console.WriteLine("whats your age?");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("hello " + name);
+            Console.WriteLine("your age is right ? " + age);
+            Console.ReadKey();
+        }
+    }
+}
