07d6d9801c860ff1174fe5c77eebf59708cc2082
Ayman Yahia <ayman.ya1997@gmail.com>
2021-09-29 12:57:44 +0300

    variables

---
+++ C#Learning/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            x = 5;
+            int z = x + 5;
+            Console.WriteLine(x);
+            Console.WriteLine(z);
+            int age=24;
+            double height = 180.2;
+            bool option=true;
+            Console.WriteLine(option + " that is .");
+            Console.WriteLine("Your height is "+height);
+            Console.WriteLine("Your age is "+age);
+            Char m = '!';
+            Console.WriteLine(m);
+            String name = "ayman";
+            Console.WriteLine(name);
+            String userName = m + name;
+            Console.WriteLine(userName);
+
+            Console.ReadKey();
+        }
+    }
+}
