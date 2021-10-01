7378c62f824a7c207ec295fb86c31224b3d50b14
Ayman Yahia <ayman.ya1997@gmail.com>
2021-10-01 20:45:59 +0300

    OOP part 2

---
+++ C#Learning/oopFund2/ArrayOfObjects/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ArrayOfObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car[] garage = { new Car("x5"), new Car("Corvette"), new Car("Lambo") };
+            foreach (Car car in garage)
+            {
+                Console.WriteLine(car.model);
+            }
+            Console.ReadKey();
+        }
+    }
+    class Car
+    {
+        public String model;
+
+        public Car(String model)
+        {
+            this.model = model;
+        }
+    }
+}
---
+++ C#Learning/oopFund2/ObjectsAsArguements/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ObjectsAsArguements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Car car1 = new Car("Mustang", "red");
+            ChangeColor(car1,"blue");
+            Console.WriteLine(car1.color);
+            Car car2 = Copy(car1);
+
+            Console.ReadKey();
+        }
+        public static void ChangeColor(Car car,String Color)
+        {
+            car.color = Color;
+        }
+        public static Car Copy(Car car)
+        {
+            return new Car(car.model, car.color);
+        }
+    }
+    class Car
+    {
+        public String model;
+        public String color;
+
+        public Car(String model, String color)
+        {
+            this.model = model;
+            this.color = color;
+        }
+    }
+}
