cd95bb4f3f0b422d8b1388a8319743d8c2fb1575
Ayman Yahia <ayman.ya1997@gmail.com>
2021-10-07 11:54:36 +0300

    OOPPart 3

---
+++ C#Learning/oop-part3/oop-part3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace oop_part3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog dog = new Dog();
+            Cat cat = new Cat();
+
+            dog.Speak();
+            cat.Speak();
+            Console.ReadKey();
+        }
+    }
+    class Animal
+    {
+        public virtual void Speak()
+        {
+            Console.WriteLine("the animal goes");
+        }
+    }
+    class Dog : Animal
+    {
+        public override void Speak()
+        {
+            Console.WriteLine("the Dog goes df");
+        }
+    }
+    class Cat : Animal
+    {
+
+    }
+}
